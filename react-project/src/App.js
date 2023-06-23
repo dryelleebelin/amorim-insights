bec04f43f556bc19a128f2f39c012445823051e6
dryelleebelin <dryellesilva07@gmail.com>
2023-06-22 23:07:03 -0300

    add alterações

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,12 +4,16 @@ import logotipoPrincipal from './assets/logotipo-principal.png';
 import lucas from './assets/lucas-removebg.png';
 import pagamentos from './assets/pagamentos.svg';
 import logotipoPreto from './assets/logotipo-preto.png';
+import logotipoAmarelo from './assets/logotipo-amarelo.png';
 
 import { FaAward, FaVideo } from 'react-icons/fa';
 import { RiTimerFlashFill } from 'react-icons/ri';
 import { BsWhatsapp, BsInstagram, BsLinkedin, BsYoutube } from 'react-icons/bs';
-import { TbCircleNumber1,TbCircleNumber2, TbCircleNumber3, TbCircleNumber4 } from 'react-icons/tb';
+import { TbCircleNumber1,TbCircleNumber2, TbCircleNumber3, TbCircleNumber4, TbCircleNumber5 } from 'react-icons/tb';
 import { IoClose } from 'react-icons/io5';
+import whatsapp from './assets/whatsapp.png';
+import discord from './assets/discord.png';
+// import videoDiscord from './assets/video-discord.mp4';
 
 import certificado1 from './assets/certificado-azure-fundamentals.png';
 import certificado2 from './assets/certificado-data-analyst.png';
@@ -20,6 +24,9 @@ import certificado5 from './assets/certificado-power-fundamentals.png';
 export default function App() {
   return (
     <div className='body'>
+      <a className='whatsapp' href='https://api.whatsapp.com/send?phone=5511964518005'>
+        <img src={whatsapp}/>
+      </a>
       <img className='logotipo-principal' src={logotipoPrincipal}/>
       <div className='headline'>
         <h2>Entre para a área que mais cresce nos últimos anos!</h2>
@@ -63,7 +70,27 @@ export default function App() {
         </tr>
       </table>
       <div className='carossel'>
-        teste carossel
+        <div className='carossel-item'>
+          <h2>Despertar da mina de ouro</h2>
+          <span>
+            <p>Universo dos dados</p>
+            <p>Onde eles estão, como se comportam, como posso me aproveitar desse momento da tecnologia?</p>
+          </span>
+        </div>
+        <div className='carossel-item'>
+          <h2>Mineração estratégica</h2>
+          <span>
+            <p>Demandas urgentes</p>
+            <p>Como o mercado está se comportando com a nova era dos dados, qual perfil procuram, o que devo estudar para alcançar essas oportunidades?</p>
+          </span>
+        </div>
+        <div className='carossel-item'>
+          <h2>Fundição do conhecimento</h2>
+          <span>
+            <p>Unindo experiências</p>
+            <p>Como usar todo meu aprendizado para aplicar no mundo dos dados?</p>
+          </span>
+        </div>
       </div>
       <div className='sobre-mim'>
         <div className='sobre-mim-texto'>
@@ -99,33 +126,33 @@ export default function App() {
         <div className='faq-item'>
           <TbCircleNumber1/>
           <div>
-            <h2>Precisa de um Super Computador?</h2>
+            <h2>Precisa de um super computador?</h2>
             <p>Não, diferente do que dizem por aí, você precisará ter um computador ou notebook com 8Gb de memória RAM, com windows 8 ou superior.</p>
           </div>
         </div>
         <div className='faq-item'>
-          <TbCircleNumber1/>
+          <TbCircleNumber2/>
           <div>
             <h2>Em quanto tempo vou aprender?</h2>
             <p>Não existe um tempo exato, cada pessoa tem uma capacidade de absorver diferente, o vai contar aqui é a sua força de vontade em colocar em prática tudo que está aprendendo.</p>
           </div>
         </div>
         <div className='faq-item'>
-          <TbCircleNumber1/>
+          <TbCircleNumber3/>
           <div>
             <h2>Como solicito suporte durante o aprendizado?</h2>
             <p>Após entrar no Power BI Flex, você ganhará acesso exclusivo ao meu grupo de mentorados no discord, onde poderá tirar suas dúvidas direto comigo.</p>
           </div>
         </div>
         <div className='faq-item'>
-          <TbCircleNumber1/>
+          <TbCircleNumber4/>
           <div>
-            <h2>As Aulas são gravadas ou AO VIVO?</h2>
+            <h2>As aulas são gravadas ou AO VIVO?</h2>
             <p>Todas as aulas são gravadas, com ressalva de novas atualizações da plataforma que posso fazer ao vivo com os alunos.</p>
           </div>
         </div>
         <div className='faq-item'>
-          <TbCircleNumber1/>
+          <TbCircleNumber5/>
           <div>
             <h2>Funciona para quem não sabe programar?</h2>
             <p>Sim, não é um requisito saber programar, você aprenderá a utilizar os recursos Drag and Drop, ou seja, arrastar e soltar, a ferramenta foi criada para ser fácil de usar.</p>
@@ -133,20 +160,21 @@ export default function App() {
         </div>
       </div>
       <div className='discord'>
-        teste video discord
+        <img src={discord}/>
+        <h2>Suporte como você nunca viu em nenhum curso.</h2>
+        {/* <video autoPlay muted loop>
+          <source src={videoDiscord} type="video/mp4"/>
+        </video> */}
       </div>
       <div className='footer'>
-        <img src={logotipoPrincipal}/>
+        <img src={logotipoAmarelo}/>
         <div className='footer-contato'>
           <a href='https://api.whatsapp.com/send?phone=5511964518005'><BsWhatsapp/></a>
           <a href='https://www.instagram.com/dados.amorim/'><BsInstagram/></a>
           <a href='https://www.linkedin.com/in/lucas-amorim-12885a118/'><BsLinkedin/></a>
           <a href='https://www.youtube.com/channel/UCgnPfc_2XXV4UYctKQSmcOg'><BsYoutube/></a>
         </div>
-        <div>
-          <p>Termos de Uso | Políticas</p>
-          <p>&#169; 2023 Todos os direitos reservados</p>
-        </div>
+        <p>&#169; 2023 Todos os direitos reservados</p>
       </div>
     </div>
   );
